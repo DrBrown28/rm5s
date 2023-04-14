10a24988bb1a346e09edb5a60a8d38f1b1a8a069
johnt1989 <johnjohnt1989@gmail.com>
2023-04-14 01:00:46 -0700

    buildscript

--- 5s.sh
+++ 5s.sh
@@ -4,42 +4,66 @@
 DIR=`readlink -f .`;
 PARENT_DIR=`readlink -f ${DIR}/..`;
 
-ARCH=arm64
-BUILD_CROSS_COMPILE=$PARENT_DIR/aarch64-linux-android-4.9/bin/aarch64-linux-android-
-KERNEL_LLVM_BIN=$PARENT_DIR/llvm-arm-toolchain-ship-10.0/bin/clang
-CLANG_TRIPLE=aarch64-linux-gnu-
-KERNEL_MAKE_ENV=""
-
-toolchain()
+aarch64-4.9()
 {
   if [ ! -d $PARENT_DIR/aarch64-linux-android-4.9 ]; then
-    git clone --branch android-9.0.0_r59 https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9 $PARENT_DIR/aarch64-linux-android-4.9
+    git clone https://github.com/johnt1989/toolchains -b aarch64-linux-android $PARENT_DIR/aarch64-linux-android-4.9
   fi
 }
 
-llvm()
+aarch64-elf-4.9()
 {
-  if [ ! -d $PARENT_DIR/llvm-arm-toolchain-ship-10.0 ]; then
-    git clone https://github.com/proprietary-stuff/llvm-arm-toolchain-ship-10.0 $PARENT_DIR/llvm-arm-toolchain-ship-10.0
+  if [ ! -d $PARENT_DIR/aarch64-linux-elf-4.9 ]; then
+    git clone https://github.com/johnt1989/toolchains -b aarch64-linux-elf $PARENT_DIR/aarch64-linux-elf-4.9
   fi
 }
+
+androideabi-4.9()
+{
+  if [ ! -d $PARENT_DIR/arm-linux-androideabi-4.9 ]; then
+    git clone https://github.com/johnt1989/toolchains -b arm-linux-androideabi $PARENT_DIR/arm-linux-androideabi-4.9
+  fi
+}
+
+clangd()
+{
+  if [ ! -d $PARENT_DIR/clang ]; then
+    git clone https://github.com/johnt1989/toolchains -b clang $PARENT_DIR/clang
+  fi
+}
+
+export CLANG_PATH=$PARENT_DIR/clang/bin/
+export ELF_PATH=$PARENT_DIR/aarch64-linux-elf-4.9/bin/
+export PATH=${CLANG_PATH}:${ELF_PATH}:${PATH}
+export CLANG_TRIPLE=aarch64-linux-gnu-
+export CROSS_COMPILE=$PARENT_DIR/aarch64-linux-android-4.9/bin/aarch64-linux-android- CC=clang CXX=clang++
+export CROSS_COMPILE_ARM32=$PARENT_DIR/arm-linux-androideabi-4.9/bin/arm-eabi-
+export KBUILD_COMPILER_STRING=$($PARENT_DIR/clang/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')
+export CXXFLAGS="$CXXFLAGS -fPIC"
+export DTC_EXT=dtc
+export ARCH=arm64
+export SUBARCH=arm64
+export KBUILD_BUILD_USER="Doc714-5S-V3"
+export KBUILD_BUILD_HOST="UBUNtU"
 
 clean()
 {
-  make $KERNEL_MAKE_ENV ARCH=arm64 CROSS_COMPILE=$BUILD_CROSS_COMPILE CC=$KERNEL_LLVM_BIN CLANG_TRIPLE=$CLANG_TRIPLE clean 
-  make $KERNEL_MAKE_ENV ARCH=arm64 CROSS_COMPILE=$BUILD_CROSS_COMPILE CC=$KERNEL_LLVM_BIN CLANG_TRIPLE=$CLANG_TRIPLE mrproper
+  make CC="ccache clang" CXX="ccache clang++" clean
+  make CC="ccache clang" CXX="ccache clang++" mrproper
   [ -d "out" ] && rm -rf out
 }
 
 build()
 {
   [ ! -d "out" ] && mkdir out
-  make -j$(nproc) -C $(pwd) O=$(pwd)/out $KERNEL_MAKE_ENV ARCH=arm64 CROSS_COMPILE=$BUILD_CROSS_COMPILE CC=$KERNEL_LLVM_BIN CLANG_TRIPLE=$CLANG_TRIPLE CFP_CC=$KERNEL_LLVM_BIN vendor/NX659J_defconfig
-  make -j$(nproc) -C $(pwd) O=$(pwd)/out $KERNEL_MAKE_ENV ARCH=arm64 CROSS_COMPILE=$BUILD_CROSS_COMPILE CC=$KERNEL_LLVM_BIN CLANG_TRIPLE=$CLANG_TRIPLE CFP_CC=$KERNEL_LLVM_BIN
+  make CC="ccache clang" CXX="ccache clang++" O=out vendor/NX659J_defconfig
+  make CC="ccache clang" CXX="ccache clang++" O=out
 
   [ -e out/arch/arm64/boot/Image.gz ] && cp out/arch/arm64/boot/Image.gz $(pwd)/out/Image.gz
   if [ -e out/arch/arm64/boot/Image.gz-dtb ]; then
     cp out/arch/arm64/boot/Image.gz-dtb $(pwd)/out/Image.gz-dtb
+
+    find out/arch/arm64/boot/dts/vendor/qcom -name '*.dtb' -exec cat {} + > $(pwd)/out/dtb
   fi
 }
 
@@ -52,6 +76,9 @@ anykernel3()
     cp out/arch/arm64/boot/Image.gz-dtb $PARENT_DIR/AnyKernel3/zImage
   elif [ -e out/arch/arm64/boot/Image ]; then
     cp out/arch/arm64/boot/Image $PARENT_DIR/AnyKernel3/zImage
+  fi
+  if [ -e out/dtb ]; then
+    cp out/dtb $PARENT_DIR/AnyKernel3/dtb
   fi
   cd $PARENT_DIR/AnyKernel3
   git reset --hard
@@ -72,8 +99,10 @@ anykernel3()
   cd $DIR
 }
 
-toolchain
-llvm
+aarch64-4.9
+aarch64-elf-4.9
+androideabi-4.9
+clangd
 clean
 build
 anykernel3
